5b06f18c9ec701155cf84859579e8c241b6b921d
iys4906 <iys4906@naver.com>
2023-08-18 17:16:15 +0900

    최초커밋

---
+++ springboot_gradle_maria/src/main/java/com/example/demo/SpringbootGradleMariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleMariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleMariaApplication.class, args);
+	}
+
+}
